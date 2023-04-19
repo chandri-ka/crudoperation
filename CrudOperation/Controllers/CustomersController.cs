e823ec91c51405b43ffc52751bd27189d12cdc67
traininglckmchangft@outlook.com <traininglkmgft@outlook.com>
2023-04-19 13:41:49 +0530

    changes

--- CrudOperation/Controllers/CustomersController.cs
+++ CrudOperation/Controllers/CustomersController.cs
@@ -41,7 +41,7 @@ namespace CrudOperation.Controllers
             db.Entry(customer1).State = System.Data.Entity.EntityState.Modified;
             db.SaveChanges();
         }
-        //This method will delete a customers
+        //This method will deleted a customer
        public string Delete(int id)
         {
             Customer customer = db.Customers.Find(id);
